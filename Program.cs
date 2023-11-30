dd29f73e4e854711fe8c82029ba05a7c2715e197
alexwincode <aleksejfeliferov@gmail.com>
2023-11-30 13:43:35 +0300

    Задача 68

--- Program.cs
+++ Program.cs
@@ -22,30 +22,63 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+// System.Console.Write("Введите число M:");
+// int nam1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите число N:");
+// int nam2 = Convert.ToInt32(Console.ReadLine());
+
+// if (nam1<= nam2)
+// {
+//     int sum = CreateSum(nam1,nam2);
+//     System.Console.WriteLine($"Сумма натуральных чисел от {nam1} до {nam2}: {sum}");
+// }
+// else 
+// {
+// System.Console.WriteLine("Ошибка: M должно быть меньше или равыно N");
+// }
+
+
+// int CreateSum(int start , int end)
+// {
+//     if (start > end)
+//     {
+//         return 0;
+//     }
+//     else
+//     {
+//         return start+CreateSum(start +1, end);
+//     }
+// }
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
 System.Console.Write("Введите число M:");
 int nam1 = Convert.ToInt32(Console.ReadLine());
 System.Console.Write("Введите число N:");
 int nam2 = Convert.ToInt32(Console.ReadLine());
+int rezaut  =Accerman(nam1,nam2);
 
-if (nam1<= nam2)
+System.Console.WriteLine($"Сумма натуральных чисел от {nam1} до {nam2}: {rezaut}");
+
+
+int Accerman(int m , int n)
 {
-    int sum = CreateSum(nam1,nam2);
-    System.Console.WriteLine($"Сумма натуральных чисел от {nam1} до {nam2}: {sum}");
+if(m == 0)
+{
+    return n+1;
 }
-else 
+else if (m>0 && n ==0)
 {
-System.Console.WriteLine("Ошибка: M должно быть меньше или равыно N");
+   return  Accerman(m-1,1);
 }
-
-
-int CreateSum(int start , int end)
+else if (m>0 && n >0)
 {
-    if (start > end)
-    {
-        return 0;
-    }
-    else
-    {
-        return start+CreateSum(start +1, end);
-    }
+    return  Accerman(m-1,Accerman(m,n-1));
+}
+else
+{
+    return 0;
+}
 }
